c14792b48542743f5c43e7aa47daac606555ecc9
Dirk Peters <rocksandwoks@gmail.com>
2017-09-26 12:05:19 -0400

    Add Container component

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,120 +1,36 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import Grid from './Grid';
-import ControlPanel from './ControlPanel';
+import Grid from "./Grid";
+import ControlPanel from "./ControlPanel";
 
-class GameBoard extends React.Component {
-  constructor() {
-    super();
+const Gameboard = props => {
+  const {
+    grid,
+    length,
+    width,
+    density,
+    speed,
+    generations,
+    handleCellClick,
+    handlePauseClick,
+    handleResetClick,
+    handleStartClick
+  } = props;
+  return (
+    <div className="game-board">
+      <ControlPanel
+        settings={{ length, width, density, speed }}
+        generations={generations}
+        handlePauseClick={handlePauseClick}
+        handleResetClick={handleResetClick}
+        handleStartClick={handleStartClick}
+      />
+      <Grid grid={grid} handleCellClick={handleCellClick} />
+    </div>
+  );
+};
 
-		this.state = {
-			grid: [],
-			length: 25,
-			width: 25,
-			density: 40,
-			speed: 1000,
-			generations: 1,
-			selected: [],
-			isPaused: false
-		}
+Gameboard.propTypes = {};
 
-		this.handleCellClick = this.handleCellClick.bind(this);
-		this.handlePauseClick = this.handlePauseClick.bind(this);
-		this.handleResetClick = this.handleResetClick.bind(this);
-		this.handleStartClick = this.handleStartClick.bind(this);
-  }
-
-  componentDidMount() {
-		const {length, width, density, speed} = this.state;
-    this.seedGrid(length, width, density);
-		setTimeout(() => this.timerID = setInterval(() => this.tick(), speed));
-  }
-
-	componentWillUnmount() {
-		clearInterval(this.timerID);
-	}
-
-	handleCellClick(yCoord, xCoord) {
-		let {grid} = this.state;
-
-		grid[yCoord][xCoord] = 2;
-		this.setState({grid});
-	}
-
-	handlePauseClick(e) {
-		e.preventDefault();
-		this.setState({isPaused: true});
-	}
-
-	handleResetClick(length, width, density, speed) {
-		console.log(length, width, density, speed)
-		clearInterval(this.timerID);
-		this.seedGrid(length, width, density);
-		this.setState({
-			generations: 1,
-			speed,
-			isPaused: false
-		});
-		setTimeout(() => this.timerID = setInterval(() => this.tick(), speed));
-	}
-
-	handleStartClick(e) {
-		e.preventDefault();
-		this.setState({isPaused: false});
-	}
-
-	seedGrid(length, width, density) {
-		const generateRandomSquare = () => Math.floor(Math.random() * 100);
-
-		const grid = Array.from({length}, () => new Array(width).fill(0))
-			.map(row => row.map(x => Math.floor(Math.random() * 100) < density ? 1 : 0));
-
-		this.setState({grid});
-	}
-
-	tick() {
-		const {grid, length, isPaused} = this.state;
-		const nextGeneration = this.state.generations + 1;
-		const oldGrid = grid.map((row, ri, g) => row.map((s, si) => row[si] > 0 ? 1 : 0))
-
-		if (isPaused) {
-			return;
-		}
-
-		this.setState({grid: oldGrid.map((row, ri, g) => {
-									 	return row.map((square, si) => {
-
-											const prevRowTotal = ri > 0 ? (g[ri-1][si-1] || 0) + g[ri-1][si] + (g[ri-1][si+1] || 0) : 0;
-											const currRowTotal = (g[ri][si-1] || 0) + (g[ri][si+1] || 0);
-											const nextRowTotal = ri+1 < length ? (g[ri+1][si-1] || 0) + g[ri+1][si] + (g[ri+1][si+1] || 0) : 0;
-											const count = prevRowTotal + currRowTotal + nextRowTotal;
-
-										  if (square && count == 2 || square && count == 3) {
-												return 1;
-											} else if (!square && (count == 3)) {
-												return 2;
-											} else {
-												return 0;
-											}
-										})
-									}),
-									generations: nextGeneration});
-	}
-
-  render() {
-		const {grid, length, width, density, speed, generations} = this.state;
-    return (
-			<div className="game-board">
-				<ControlPanel
-					settings={{length, width, density, speed}}
-					generations={generations}
-					handlePauseClick={this.handlePauseClick}
-					handleResetClick={this.handleResetClick}
-					handleStartClick={this.handleStartClick}
-			  />
-				<Grid grid={grid} handleCellClick={this.handleCellClick} />
-			</div>
-    );
-  }
-}
+export default Gameboard;
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Grid = ({grid, handleCellClick}) =>
+const Grid = ({ grid, handleCellClick }) => (
   <div className="grid">
-	{grid.map((row, ri) => <div className="row">{row.map((c, ci) => <div className={'cell'+c} onClick={handleCellClick.bind(this, ri, ci)}>{c}</div>)}</div>)}
-	</div>
+    {grid.map((row, ri) => (
+      <div className="row">
+        {row.map((c, ci) => (
+          <div
+            className={"cell" + c}
+            onClick={handleCellClick.bind(this, ri, ci)}
+          >
+            {c}
+          </div>
+        ))}
+      </div>
+    ))}
+  </div>
+);
 
 Grid.propTypes = {
-	grid: PropTypes.array.isRequired,
-	handleCellClick: PropTypes.func.isRequired
-}
+  grid: PropTypes.array.isRequired,
+  handleCellClick: PropTypes.func.isRequired
+};
+
+export default Grid;
